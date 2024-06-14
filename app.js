e37eb23f04f3565e49936090af6228a2c3c068a4
Azer-Novruzov <azernovruzov200@gmail.com>
2024-06-14 19:00:30 +0400

    js 2 home work

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let fullName = "Azer Novruzov";
-console.log(fullName.split(" ") [0] [0]);
-console.log(fullName.split(" ") [1] [0]);
+console.log(fullName.split(" ")[0][0]);
+console.log(fullName.split(" ")[1][0]);
 console.log(fullName.split(" ")[1] + " " + fullName.split(" ")[0]);
 // console.log(fullName.split(" ").reverse().join(" "));bunu kecmemisiy ama yenede saxladimki hansi daha yaxsidi ?
